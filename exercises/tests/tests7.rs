15b36f11ade9b7d80e79fc790ceb1f1da40e3c39
xuanau <95044645+xuanau@users.noreply.github.com>
2023-10-22 21:52:49 +0800

    Update tests7.rs

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -35,21 +35,11 @@
 // hint.
 
 
-
-fn main() {}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
-    fn test_success() {
-        let timestamp = std::time::SystemTime::now()
-            .duration_since(std::time::UNIX_EPOCH)
-            .unwrap()
-            .as_secs();
-        let s = std::env::var("TEST_FOO").unwrap();
-        let e: u64 = s.parse().unwrap();
-        assert!(timestamp >= e && timestamp < e + 10);
+    fn you_can_assert() {
+        let a = 5;
+        assert!(a>0,"a is bigger than 0");
     }
 }
