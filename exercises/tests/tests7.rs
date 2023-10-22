9eddc915451ec81f74f01fd7e31384f21d0353bc
xuanau <95044645+xuanau@users.noreply.github.com>
2023-10-22 21:32:11 +0800

    Update tests7.rs

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -33,16 +33,8 @@
 //
 // Execute `rustlings hint tests7` or use the `hint` watch subcommand for a
 // hint.
-// tests8.rs
-//
-// This execrise shares `build.rs` with the previous exercise.
-// You need to add some code to `build.rs` to make both this exercise and
-// the previous one work.
-//
-// Execute `rustlings hint tests8` or use the `hint` watch subcommand for a
-// hint.
 
-// I AM NOT DONE
+
 
 fn main() {}
 
@@ -52,9 +44,12 @@ mod tests {
 
     #[test]
     fn test_success() {
-        #[cfg(feature = "pass")]
-        return;
-
-        panic!("no cfg set");
+        let timestamp = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        let s = std::env::var("TEST_FOO").unwrap();
+        let e: u64 = s.parse().unwrap();
+        assert!(timestamp >= e && timestamp < e + 10);
     }
 }
