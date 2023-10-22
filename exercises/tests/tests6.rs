50bfb02bde08ac57cec19b5d778a757538ba4e06
xuanau <95044645+xuanau@users.noreply.github.com>
2023-10-22 20:59:35 +0800

    Update tests6.rs

--- exercises/tests/tests6.rs
+++ exercises/tests/tests6.rs
@@ -7,22 +7,26 @@
 // Execute `rustlings hint tests6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 struct Foo {
     a: u128,
     b: Option<String>,
 }
 
-/// # Safety
-///
-/// The `ptr` must contain an owned box of `Foo`.
-unsafe fn raw_pointer_to_box(ptr: *mut Foo) -> Box<Foo> {
-    // SAFETY: The `ptr` contains an owned box of `Foo` by contract. We
-    // simply reconstruct the box from that pointer.
-    let mut ret: Box<Foo> = unsafe { ??? };
-    todo!("The rest of the code goes here")
+fn raw_pointer_to_box(address: usize) -> Box<Foo> {
+    // address is a pointer that points to heap.
+    let ptr = address as *mut Foo;
+    // construct Box from this address, and modify Foo's b field to 
+    // the string "hello"
+    let mut boxed_foo = unsafe{
+        Box::from_raw(ptr)
+    };
+    boxed_foo.b = Some("hello".to_owned());
+    boxed_foo
+    
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -31,15 +35,20 @@ mod tests {
 
     #[test]
     fn test_success() {
-        let data = Box::new(Foo { a: 1, b: None });
+        let nanos = Instant::now().duration_since(Instant::now().checked_sub(std::time::Duration::new(0, 1)).unwrap()).as_nanos();
+
+        let data = Box::new(Foo{
+            a: nanos,
+            b: None
+        });
 
         let ptr_1 = &data.a as *const u128 as usize;
-        // SAFETY: We pass an owned box of `Foo`.
-        let ret = unsafe { raw_pointer_to_box(Box::into_raw(data)) };
+        let ret = raw_pointer_to_box(Box::into_raw(data) as usize);
 
         let ptr_2 = &ret.a as *const u128 as usize;
 
         assert!(ptr_1 == ptr_2);
         assert!(ret.b == Some("hello".to_owned()));
+
     }
 }
