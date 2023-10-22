04f49684f60666784a90bd4162508f55782d8d91
xuanau <95044645+xuanau@users.noreply.github.com>
2023-10-22 21:31:36 +0800

    Update tests7.rs

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -33,19 +33,18 @@
 //
 // Execute `rustlings hint tests7` or use the `hint` watch subcommand for a
 // hint.
+// tests8.rs
+//
+// This execrise shares `build.rs` with the previous exercise.
+// You need to add some code to `build.rs` to make both this exercise and
+// the previous one work.
+//
+// Execute `rustlings hint tests8` or use the `hint` watch subcommand for a
+// hint.
 
-/// # Safety
-///
-/// The `address` must contain a mutable reference to a valid `u32` value.
-unsafe fn modify_by_address(address: usize) {
-    // TODO: Fill your safety notice of the code block below to match your
-    // code's behavior and the contract of this function. You may use the
-    // comment of the test below as your format reference.
-    let ptr = address as *mut u32;
-    unsafe {
-        *ptr = 0xAABBCCDD
-    }
-}
+// I AM NOT DONE
+
+fn main() {}
 
 #[cfg(test)]
 mod tests {
@@ -53,10 +52,9 @@ mod tests {
 
     #[test]
     fn test_success() {
-        let mut t: u32 = 0x12345678;
-        // SAFETY: The address is guaranteed to be valid and contains
-        // a unique reference to a `u32` local variable.
-        unsafe { modify_by_address(&mut t as *mut u32 as usize) };
-        assert!(t == 0xAABBCCDD);
+        #[cfg(feature = "pass")]
+        return;
+
+        panic!("no cfg set");
     }
 }
