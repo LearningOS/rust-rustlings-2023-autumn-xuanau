e1946f33b69e3339ca0e78d804c8fd04ee2d505a
xuanau <95044645+xuanau@users.noreply.github.com>
2023-10-22 21:16:22 +0800

    Update tests7.rs

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -33,11 +33,24 @@
 //
 // Execute `rustlings hint tests7` or use the `hint` watch subcommand for a
 // hint.
-
 use std::env;
 use std::time::{SystemTime,UNIX_EPOCH};
 
-fn main() {}
+
+fn main() {
+    // let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_secs();
+    // let test_foo_value = timestamp+5;
+    // env::set_var("TEST_FOO",test_foo_value.to_string());
+    // println!("cargo:rerun-if-changed=build.rs");
+    // println!("rcargo:rustc-env=TEST_FOO={}",test_foo_value);
+    // if test_foo_value %2 ==0{
+    //     println!("cargo:rustc-cfg=feature=\"pass\"");
+        
+    // }
+
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -47,5 +60,13 @@ mod tests {
         let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
         let e:u64 = timestamp;
         assert! (timestamp >= e && timestamp < e + 10);
+        
+        // // #[cfg(feature = "pass")]
+        // let timestamp:u64 =10;
+        // let e:u64 = 10;
+        assert! (timestamp>=e&&timestamp<e+10);
+        // return;
+
+        // panic!("no cfg set");
     }
 }
