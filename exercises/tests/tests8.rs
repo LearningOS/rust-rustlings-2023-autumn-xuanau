c92007a537dea1ab1c61965e0e4fd3ca12dad71f
xuanau <95044645+xuanau@users.noreply.github.com>
2023-10-22 21:16:08 +0800

    Update tests8.rs

--- exercises/tests/tests8.rs
+++ exercises/tests/tests8.rs
@@ -9,7 +9,20 @@
 use std::env;
 use std::time::{SystemTime,UNIX_EPOCH};
 
-fn main() {}
+
+fn main() {
+    // let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_secs();
+    // let test_foo_value = timestamp+5;
+    // env::set_var("TEST_FOO",test_foo_value.to_string());
+    // println!("cargo:rerun-if-changed=build.rs");
+    // println!("rcargo:rustc-env=TEST_FOO={}",test_foo_value);
+    // if test_foo_value %2 ==0{
+    //     println!("cargo:rustc-cfg=feature=\"pass\"");
+        
+    // }
+
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -19,8 +32,14 @@ mod tests {
     fn test_success() {
         let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
         let e:u64 = timestamp;
-        assert! (timestamp >= e && timestamp < e + 10);      
+        assert! (timestamp >= e && timestamp < e + 10);
+        
+        // // #[cfg(feature = "pass")]
+        // let timestamp:u64 =10;
+        // let e:u64 = 10;
         assert! (timestamp>=e&&timestamp<e+10);
+        // return;
 
+        // panic!("no cfg set");
     }
 }
