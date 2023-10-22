d2b613f6517befa88e56f3e9794b94aab713ddc0
xuanau <95044645+xuanau@users.noreply.github.com>
2023-10-22 21:57:26 +0800

    Update build.rs

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -5,26 +5,51 @@ use std::env;
 use std::time::{SystemTime,UNIX_EPOCH};
 
 fn main() {
-    // In tests7, we should set up an environment variable
-    // called `TEST_FOO`. Print in the standard output to let
-    // Cargo do it.
     let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_secs();
     let test_foo_value = timestamp+5;
     env::set_var("TEST_FOO",test_foo_value.to_string());
     println!("cargo:return-if-changed=build.rs");
     println!("rcargo:zustc-env=TEST_FOO={}",test_foo_value);
-    );
-
-    // In tests8, we should enable "pass" feature to make the
-    // testcase return early. Fill in the command to tell
-    // Cargo about that.
-    use std::env;
-    use std::time::{SystemTime,UNIX_EPOCH};
-    let tempstamp = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_secs();
-    let test_foo_value = timestamp+5;
-    env::set_var("TEST_FOO",test_foo_value.to_string());
-    println!("cargo:return-if-changed=build.rs");
-    println!("rcargo:zustc-env=TEST_FOO={}",test_foo_value);
-    if test_foo_value %2 ==0{
-        println!("cargo:rustc-cfg=feature=\"pass\"");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_success() {
+        let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
+        let s = std::env::var("TEST_FOO").unwrap();
+        let e:u64 = s.parse().unwrap();
+        assert! (timestamp >= e && timestamp < e + 10);
+    }
+}
+
+
+// use std::env;
+// use std::time::{SystemTime,UNIX_EPOCH};
+
+// fn main() {
+//     // In tests7, we should set up an environment variable
+//     // called `TEST_FOO`. Print in the standard output to let
+//     // Cargo do it.
+//     let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_secs();
+//     let test_foo_value = timestamp+5;
+//     env::set_var("TEST_FOO",test_foo_value.to_string());
+//     println!("cargo:return-if-changed=build.rs");
+//     println!("rcargo:zustc-env=TEST_FOO={}",test_foo_value);
+//     );
+
+//     // In tests8, we should enable "pass" feature to make the
+//     // testcase return early. Fill in the command to tell
+//     // Cargo about that.
+//     use std::env;
+//     use std::time::{SystemTime,UNIX_EPOCH};
+//     let tempstamp = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_secs();
+//     let test_foo_value = timestamp+5;
+//     env::set_var("TEST_FOO",test_foo_value.to_string());
+//     println!("cargo:return-if-changed=build.rs");
+//     println!("rcargo:zustc-env=TEST_FOO={}",test_foo_value);
+//     if test_foo_value %2 ==0{
+//         println!("cargo:rustc-cfg=feature=\"pass\"");
+// }
